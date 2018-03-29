edcadbca4181d23624aa071b7aff2dc0ef3bec23
Tim Gieseking <tgieseking@atomicdust.com>
2018-03-29 13:38:37 -0500

    fix textdomain

--- basetheme-helper-plugin.php
+++ basetheme-helper-plugin.php
@@ -8,7 +8,7 @@
  * Author URI:		http://timgweb.com/
  * License:			GPL-2.0+
  * License URI:		http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:		concordia-events-import
+ * Text Domain:		basetheme
  */
 
 namespace ATMDST;
