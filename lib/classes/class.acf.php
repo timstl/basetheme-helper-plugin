3643fca36e26d5d2278d50239f8e9133e156a204
Tim Gieseking <tgieseking@atomicdust.com>
2018-09-01 10:49:15 -0500

    remove logo group from acf fields; for some reason array method is not working when field added via php

--- lib/classes/class.acf.php
+++ lib/classes/class.acf.php
@@ -228,54 +228,6 @@ class ACF {
 		if( function_exists('acf_add_local_field_group') ):
 
 			acf_add_local_field_group(array(
-				'key' => 'group_5b3a831c722aa',
-				'title' => 'Brand',
-				'fields' => array(
-					array(
-						'key' => 'field_5b3a83224205a',
-						'label' => 'Logo',
-						'name' => 'logo',
-						'type' => 'image',
-						'instructions' => 'SVG file.',
-						'required' => 0,
-						'conditional_logic' => 0,
-						'wrapper' => array(
-							'width' => '33',
-							'class' => '',
-							'id' => '',
-						),
-						'return_format' => 'array',
-						'preview_size' => 'thumbnail',
-						'library' => 'all',
-						'min_width' => '',
-						'min_height' => '',
-						'min_size' => '',
-						'max_width' => '',
-						'max_height' => '',
-						'max_size' => '',
-						'mime_types' => '',
-					),
-				),
-				'location' => array(
-					array(
-						array(
-							'param' => 'options_page',
-							'operator' => '==',
-							'value' => 'site-general-settings',
-						),
-					),
-				),
-				'menu_order' => 0,
-				'position' => 'normal',
-				'style' => 'default',
-				'label_placement' => 'top',
-				'instruction_placement' => 'label',
-				'hide_on_screen' => '',
-				'active' => 1,
-				'description' => '',
-			));
-
-			acf_add_local_field_group(array(
 				'key' => 'group_5b3e46a826f7b',
 				'title' => 'Copyright',
 				'fields' => array(
