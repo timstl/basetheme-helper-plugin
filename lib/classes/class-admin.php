cce1137e8b26a4ff95cb225f0d6acbf6fd3a28c3
Tim Gieseking <tgieseking@atomicdust.com>
2018-09-14 16:29:08 -0500

    more file formatting

--- lib/classes/class-admin.php
+++ lib/classes/class-admin.php
@@ -1,17 +1,34 @@
 <?php
 /**
- * Do some cleanup when this plugin is first activated.
+ * Admin functions, other than ACF.
+ *
+ * @package WordPress
+ * @subpackage Basetheme Helper Plugin
+ * @since 1.0
+ * @version 1.2
  */
+
 namespace BTH\Lib\Classes;
 
+/**
+ * Admin class
+ */
 class Admin {
 
+	/**
+	 * __construct()
+	 */
 	public function __construct() {
 		add_action( 'admin_notices', array( $this, 'admin_notices' ) );
 	}
 
+	/**
+	 * Add notices to the admin.
+	 */
 	public function admin_notices() {
-		if ( current_user_can( 'manage_options' ) && '0' == get_option( 'blog_public' ) ) {
+		if ( current_user_can( 'manage_options' ) &&
+			( '0' === get_option( 'blog_public' ) || 0 === get_option( 'blog_public' ) )
+			) {
 			echo '<div class="error"><p>Search engines are currently blocked.</p></div>';
 		}
 	}
--- lib/classes/class-cleanup.php
+++ lib/classes/class-cleanup.php
@@ -1,32 +1,46 @@
 <?php
 /**
- * Do some cleanup when this plugin is first activated.
+ * Do some cleanup when WordPress loads.
+ *
+ * @package WordPress
+ * @subpackage Basetheme Helper Plugin
+ * @since 1.0
+ * @version 1.2
  */
+
 namespace BTH\Lib\Classes;
 
+/**
+ * Cleanup Class.
+ */
 class Cleanup {
 
+	/**
+	 * __construct()
+	 */
 	public function __construct() {
-		/* Remove some excess WordPress info from the head */
-		remove_action( 'wp_head', 'rsd_link' ); // remove really simple discovery link
-		remove_action( 'wp_head', 'wp_generator' ); // remove WordPress version
+		remove_action( 'wp_head', 'rsd_link' ); // Remove really simple discovery link.
+		remove_action( 'wp_head', 'wp_generator' ); // Remove WordPress version.
 
-		remove_action( 'wp_head', 'feed_links', 2 ); // remove rss feed links (make sure you add them in yourself if youre using feedblitz or an rss service)
-		remove_action( 'wp_head', 'feed_links_extra', 3 ); // removes all extra rss feed links
+		remove_action( 'wp_head', 'feed_links', 2 ); // Remove rss feed links (make sure you add them in yourself if youre using feedblitz or an rss service).
+		remove_action( 'wp_head', 'feed_links_extra', 3 ); // Removes all extra rss feed links.
 
-		remove_action( 'wp_head', 'index_rel_link' ); // remove link to index page
-		remove_action( 'wp_head', 'wlwmanifest_link' ); // remove wlwmanifest.xml (needed to support windows live writer)
+		remove_action( 'wp_head', 'index_rel_link' ); // Remove link to index page.
+		remove_action( 'wp_head', 'wlwmanifest_link' ); // Remove wlwmanifest.xml (needed to support windows live writer).
 
-		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // remove random post link
-		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // remove parent post link
-		remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // remove the next and previous post links
+		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // Remove random post link.
+		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // Remove parent post link.
+		remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // rRemove the next and previous post links.
 		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 
-		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 ); // Remove shortlink from wp_head.
 
-		add_filter( 'the_generator', array( $this, 'version_removal' ) );
+		add_filter( 'the_generator', array( $this, 'version_removal' ) ); // Remove the version.
 	}
 
+	/**
+	 * Return empty string to remove version.
+	 */
 	public function version_removal() {
 		return '';
 	}
