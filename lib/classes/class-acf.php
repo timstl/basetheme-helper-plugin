ebce40f4842ef058dc99c9263c8a9b783f2bc5c9
Tim Gieseking <tgieseking@atomicdust.com>
2019-01-04 13:33:32 -0600

    re-add logo section to admin

--- lib/classes/class-acf.php
+++ lib/classes/class-acf.php
@@ -87,6 +87,57 @@ class ACF {
 	 * Create ACF fields.
 	 */
 	public function create_acf_fields() {
+		if( function_exists('acf_add_local_field_group') ):
+
+		acf_add_local_field_group(array(
+			'key' => 'group_5c2fa9a9ee839',
+			'title' => 'Brand',
+			'fields' => array(
+				array(
+					'key' => 'field_5c2fa9aea6b3a',
+					'label' => 'Logo',
+					'name' => 'logo',
+					'type' => 'image',
+					'instructions' => 'SVG file',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'array',
+					'preview_size' => 'thumbnail',
+					'library' => 'all',
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'options_page',
+						'operator' => '==',
+						'value' => 'site-general-settings',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => 1,
+			'description' => '',
+		));
+
+		endif;
 		if ( function_exists( 'acf_add_local_field_group' ) ) :
 
 			acf_add_local_field_group( array(
